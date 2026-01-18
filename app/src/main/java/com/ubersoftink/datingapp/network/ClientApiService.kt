3899ebf0ea37d6cc2397cb71aef12b115e27dc4a
sas <sashakan2005@gmail.com>
2026-01-18 16:07:15 +0300

    Optimize imports

--- app/src/main/java/com/ubersoftink/datingapp/network/ClientApiService.kt
+++ app/src/main/java/com/ubersoftink/datingapp/network/ClientApiService.kt
@@ -1,11 +1,7 @@
 package com.ubersoftink.datingapp.network
 
 import com.ubersoftink.datingapp.data.models.CatResponse
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 interface ClientApiService {
